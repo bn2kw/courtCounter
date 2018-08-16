7ebcbf92e058ad6077b20af808e8ef4f440a413d
Abhishek <gaikwadabhishek978@gmail.com>
2018-08-16 15:39:32 +0530

    Update CourtCounterTest.java

--- app/src/main/java/com/example/abhishek/courtcounter/CourtCounterTest.java
+++ app/src/main/java/com/example/abhishek/courtcounter/CourtCounterTest.java
@@ -1,21 +1,12 @@
 package com.example.abhishek.courtcounter;
 
-import java.net.MalformedURLException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-
+import org.testng.annotations.Test;
 
 public class CourtCounterTest {
 
