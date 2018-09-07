465b8be915f0d55b07b9881675304f7f01be2503
Abhishek <gaikwadabhishek978@gmail.com>
2018-09-07 20:46:49 +0530

    Update MainActivityTest.java

--- app/src/androidTest/java/com/example/abhishek/courtcounter/MainActivityTest.java
+++ app/src/androidTest/java/com/example/abhishek/courtcounter/MainActivityTest.java
@@ -48,7 +48,7 @@ public class MainActivityTest {
     }
 
     private void press(int id){
-        onView(withId(id)).perform(click());
+        onView(withId(id)).perform(scrollTo(),click());
     }
 
     private void checkResult(String desired){
