409355917cd1ecbdffa87e29cac1c007764b0c8d
Abhishek <gaikwadabhishek978@gmail.com>
2018-09-09 15:15:05 +0530

    Update MainActivityTest.java

--- app/src/androidTest/java/com/example/abhishek/courtcounter/MainActivityTest.java
+++ app/src/androidTest/java/com/example/abhishek/courtcounter/MainActivityTest.java
@@ -45,7 +45,8 @@ public class MainActivityTest {
     }
 
     private void press(int id){
-        onView(withId(id)).check(matches(isCompletelyDisplayed())).perform(click());
+        //onView(withId(id)).check(matches(isCompletelyDisplayed())).perform(click());
+        onData(withId(id)).perform(click());
     }
 
     private void checkResult(String desired){
