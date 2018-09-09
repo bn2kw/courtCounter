30f0539510aaf20b3b77a6957d6f5c13cb27935f
Abhishek <gaikwadabhishek978@gmail.com>
2018-09-09 15:05:31 +0530

    Update MainActivityTest.java

--- app/src/androidTest/java/com/example/abhishek/courtcounter/MainActivityTest.java
+++ app/src/androidTest/java/com/example/abhishek/courtcounter/MainActivityTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
